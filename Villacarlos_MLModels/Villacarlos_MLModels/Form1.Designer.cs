2e5fa32810b8dd7253a07c58c5ad3d953ec5b199
Ada Pauline Villacarlos <47477123+oohada@users.noreply.github.com>
2021-11-05 09:56:41 +0800

    first commit

---
+++ Villacarlos_MLModels/Villacarlos_MLModels/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿
+namespace Villacarlos_MLModels
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(83, 102);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(129, 60);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Create Learning Model";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(83, 264);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(129, 60);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Train Model (AND)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(83, 358);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(129, 60);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Test Model (AND)";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(281, 122);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(129, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(281, 209);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(129, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(465, 165);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(129, 22);
+            this.textBox3.TabIndex = 5;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(281, 264);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(129, 60);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Train Model (OR)";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(281, 358);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(129, 60);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Test Model (OR)";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ Villacarlos_MLModels/Villacarlos_MLModels/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Backprop; 
+
+namespace Villacarlos_MLModels
+{
+    public partial class Form1 : Form
+    {
+        NeuralNet nn; 
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            nn = new NeuralNet(2,5, 1); 
+            // x (input) - meaning it can put x numbers of input which are interconnected to the neural compute neurons; 1 input is connected to each neurons,
+            // neural compute neurons - it has n neural neuros which is also connected to the output, if you add more neuronet, it would have more time complexity
+            // y(output) -- y number of output  
+            // neural compute neurons  == braincells or brain neurons in terms of the brain, the more you have it would memorize more. 
+
+            //Machine Learning - 3 layers only 
+            //Deep Learning - if there is >3 layers; eg. CNN since it consist 15 - n layers. 
+
+
+        }
+
+        //1st thing you do after you created a model. 
+        //In this phase, you tell the neuronet what inputs do you want and what is the corresponding output that you want to receive. 
+        //e.g. if you want to input 0,0, your ouput would be 0 for an AND operation. 
+
+        //This is an example of supervised classification model since you are teaching the model what appropriate inputs should mertis a correct output for the AND logic operation. 
+
+        //Overfit - training the model a lot like with the 0 AND 0 
+        //Underfit - you train the model less like having a less epoch or less neuronet  
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            for (int c = 0; c <500; c++)
+            {
+                // 1 AND 1 - 1 training model 
+                nn.setInputs(0, 1.0); //index 0 means the position of the input
+                nn.setInputs(1, 1.0); //index 1 position so if there is a third one, it would be nn.setInputs(2,input); 
+                nn.setDesiredOutput(0, 1.0); //only 1 ouput so only index 0 
+                nn.learn(); //understand what your set items, memorize and understand. 
+
+                // 0 AND 1 
+                nn.setInputs(0, 0.0);
+                nn.setInputs(1, 1.0);
+                nn.setDesiredOutput(0, 0.0);
+                nn.learn();
+
+                //1 AND 0 
+                nn.setInputs(0, 1.0);
+                nn.setInputs(1, 0.0);
+                nn.setDesiredOutput(0, 0.0);
+                nn.learn();
+
+                //0 AND 0 
+                nn.setInputs(0, 0.0);
+                nn.setInputs(1, 0.0);
+                nn.setDesiredOutput(0, 0.0);
+                nn.learn();
+
+
+                //this is considered as one iteration of training or 1 epoch since the loop is set to 500 it means it has 500 epochs 
+            }
+            //After training you can now put inputs and test it. 
+        }
+
+        //You need to find the right size model for the system and consider the times how many times you would train the system 
+        private void button3_Click(object sender, EventArgs e)
+        {
+            nn.setInputs(0, Convert.ToDouble(textBox1.Text));
+            nn.setInputs(1, Convert.ToDouble(textBox2.Text));
+            nn.run();
+            textBox3.Text = "" + nn.getOuputData(0);
+        }
+        //If the output displays >=0.5 it is represented as True or logic 1 but if it is <0.5 it is represented as False or logic 0. 
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            for (int c = 0; c < 1000; c++)
+            {
+                // 1 AND 1 
+                nn.setInputs(0, 1.0);
+                nn.setInputs(1, 1.0);
+                nn.setDesiredOutput(0, 1.0);
+                nn.learn();
+
+                // 0 AND 1 
+                nn.setInputs(0, 0.0);
+                nn.setInputs(1, 1.0);
+                nn.setDesiredOutput(0, 1.0);
+                nn.learn();
+
+                //1 AND 0 
+                nn.setInputs(0, 1.0);
+                nn.setInputs(1, 0.0);
+                nn.setDesiredOutput(0, 1.0);
+                nn.learn();
+
+                //0 AND 0 
+                nn.setInputs(0, 0.0);
+                nn.setInputs(1, 0.0);
+                nn.setDesiredOutput(0, 0.0);
+                nn.learn();
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            nn.setInputs(0, Convert.ToDouble(textBox1.Text));
+            nn.setInputs(1, Convert.ToDouble(textBox2.Text));
+            nn.run();
+            textBox3.Text = "" + nn.getOuputData(0);
+        }
+    }
+}
