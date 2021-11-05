a6d9ce95f1da74e3c7255f61ce6245291549b4d5
Ada Pauline Villacarlos <47477123+oohada@users.noreply.github.com>
2021-11-05 09:57:49 +0800

    first commit

---
+++ VillacarlosActivity1/VillacarlosActivity1/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace VillacarlosActivity1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox1.Location = new System.Drawing.Point(37, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(691, 366);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(431, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Turn on";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(523, 416);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Turn off";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Activity 1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ VillacarlosActivity1/VillacarlosActivity1/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace VillacarlosActivity1
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+        int x = 10;
+        int y = 10;
+        int heightR = 2;
+        int heightR2 = 200;
+        int flag = 0;
+        int flag2 = 0;
+        int flag3 = 0;
+        int flag4 = 0; 
+
+        int s1 = 50;
+        int s2 = 20;
+   
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Paint(object sender, PaintEventArgs e)
+        {
+         
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            // Create solid brush.
+            SolidBrush redBrush = new SolidBrush(Color.Red);
+            SolidBrush yellowBrush = new SolidBrush(Color.Yellow);
+            SolidBrush greenBrush = new SolidBrush(Color.Green);
+            g = e.Graphics;
+            g.FillRectangle(greenBrush, s1, s2, 250, heightR2);
+            g.FillEllipse(redBrush,100,50,x,y);
+            g.FillRectangle(yellowBrush, 110, 130, 120, heightR);
+            g.FillEllipse(redBrush, 200, 50, x, y);
+         
+
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            /* EYES */ 
+            x = x + 3;
+            y = y + 3;
+            if (x > 50)
+            {
+                x -= 10;
+            }
+
+            if (y > 50)
+            {
+                y -= 10;
+            }
+
+            /* MOUTH */ 
+            if (flag==0)
+            {
+                if (heightR < 50)
+                {
+                    heightR += 5;
+                }
+                
+                flag = 0;
+            }
+
+            if (heightR > 50)
+                flag = 1;
+
+            if (flag==1)
+            {
+                heightR -= 2;
+                if (heightR == 2)
+                    flag = 0;
+            }
+
+
+            /* HEAD */
+        if(flag4 == 0)
+            {
+             if(flag2 == 0)
+                       {
+                            s1 += 10;
+                            if (s1 == 100)
+                            {
+                                flag2 = 1; 
+                            }   
+                        }
+
+                      if(flag2 == 1)
+                        {
+                            s1 -= 10; 
+                            if(s1 == 40)
+                            {
+                                flag2 = 0;
+                                flag4 = 1;
+                            }
+                        }
+            }
+         
+
+           if(flag4 == 1)
+            {
+                if(flag3 == 0)
+                {
+                    if (heightR2 < 250)
+                    {
+                        heightR2 += 10;
+                    }
+                      
+                    flag3 = 0; 
+                }
+
+                if(heightR2 == 250)
+                {
+                    flag3 = 1; 
+                }
+
+                if (flag3 == 1)
+                {
+                    heightR2 -= 10; 
+                    if(heightR2 < 200)
+                    {
+                        flag3 = 0;
+                        flag4 = 0; 
+                    }
+                }
+
+            }
+
+
+
+
+            /* 
+             * 
+             * Rectangle : 50, 20, 250, 200
+             * Line: 200, 150, 150, 150
+             * g.DrawRectangle(Pens.White, 50, 20, 250, 200);
+             * g.DrawLine(Pens.White, 200, 150, 150, 150);
+             */
+
+            this.Refresh();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            timer1.Enabled = true;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            timer1.Enabled = false; 
+        }
+    }
+}
