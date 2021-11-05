1f24fe9413a07e194fb373bd450ae572dc2087c2
Ada Pauline Villacarlos <47477123+oohada@users.noreply.github.com>
2021-11-05 09:53:56 +0800

    first commit

---
+++ DipCS/DipCS/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿
+namespace DipCS
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.button7 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(36, 46);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(392, 46);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(300, 300);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(36, 373);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(125, 39);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Load Image";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(187, 373);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 39);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Save Image";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(358, 373);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 39);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Basic Copy";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(598, 373);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(94, 39);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "Grayscale";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(473, 373);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 39);
+            this.button5.TabIndex = 6;
+            this.button5.Text = "Invert";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(598, 427);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(94, 39);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "Histogram";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(358, 427);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(100, 39);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "Sepia";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(791, 506);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Villacarlos - Image Processing";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button button7;
+    }
+}
+
---
+++ DipCS/DipCS/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DipCS
+{
+    public partial class Form1 : Form
+    {
+
+        Bitmap loadimage, resultimage;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            loadimage = new Bitmap(openFileDialog1.FileName);
+            pictureBox1.Image = loadimage;
+
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            resultimage = new Bitmap(loadimage.Width, loadimage.Height); 
+            for(int x=0; x<loadimage.Width; x++)
+            {
+                for(int y=0; y < loadimage.Height; y++)
+                {
+                    Color pixel = loadimage.GetPixel(x, y);
+                    resultimage.SetPixel(x, y, pixel); 
+                }
+            }
+            pictureBox2.Image = resultimage;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            resultimage = new Bitmap(loadimage.Width, loadimage.Height);
+            for (int x = 0; x < loadimage.Width; x++)
+            {
+                for (int y = 0; y < loadimage.Height; y++)
+                {
+                    Color pixel = loadimage.GetPixel(x, y);
+                    int grey = (pixel.R + pixel.G + pixel.B) / 3;
+                    resultimage.SetPixel(x, y, Color.FromArgb(grey,grey,grey));
+                }
+            }
+            pictureBox2.Image = resultimage;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            resultimage = new Bitmap(loadimage.Width, loadimage.Height);
+            for (int x = 0; x < loadimage.Width; x++)
+            {
+                for (int y = 0; y < loadimage.Height; y++)
+                {
+                    Color pixel = loadimage.GetPixel(x, y);
+                    resultimage.SetPixel(x, y, Color.FromArgb(255-pixel.R, 255-pixel.G, 255-pixel.B));
+                }
+            }
+            pictureBox2.Image = resultimage;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            resultimage = new Bitmap(loadimage.Width, loadimage.Height);
+
+            for (int x = 0; x < loadimage.Width; x++)
+            {
+                for (int y = 0; y < loadimage.Height; y++)
+                {
+                    Color pixel = loadimage.GetPixel(x, y);
+                    int grey = (pixel.R + pixel.G + pixel.B) / 3;
+                    resultimage.SetPixel(x, y, Color.FromArgb(grey, grey, grey));
+                }
+            }
+
+            Color sample; 
+            int[] histdata = new int[256];
+            for (int x = 0; x < loadimage.Width; x++)
+            {
+                for (int y = 0; y < loadimage.Height; y++)
+                {
+                    sample = resultimage.GetPixel(x, y);
+                    histdata[sample.R]++; 
+                }
+            }
+            Bitmap mydata = new Bitmap(256, 800);
+            for (int x = 0; x < 256; x++)
+            {
+                for (int y = 0; y < 800; y++)
+                {
+                    mydata.SetPixel(x, y, Color.White); 
+                }
+            }
+
+            //plot histdata
+            for (int x = 0; x < 256; x++)
+            {
+                for (int y = 0; y < Math.Min(histdata[x]/5,800); y++)
+                {
+                    mydata.SetPixel(x, 799-y, Color.Black);
+                }
+            }
+            pictureBox2.Image = mydata;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.ShowDialog();
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            resultimage.Save(saveFileDialog1.FileName); 
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            resultimage = new Bitmap(loadimage.Width, loadimage.Height);
+            for (int x = 0; x < loadimage.Width; x++)
+            {
+                for (int y = 0; y < loadimage.Height; y++)
+                {
+                    Color pixel = loadimage.GetPixel(x, y);
+
+                    int r = pixel.R;
+                    int g = pixel.G;
+                    int b = pixel.B;
+
+                    int redS = (int)(0.393 * r + 0.769 * g + 0.189 * b);
+                    int greenS = (int)(0.349 * r + 0.686 * g + 0.168 * b);
+                    int blueS = (int)(0.272 * r + 0.534 * g + 0.131 * b); 
+
+
+                    if(redS > 255)
+                    {
+                        r = 255; 
+                    } else
+                    {
+                        r = redS;
+                    }
+
+                    if(greenS > 255)
+                    {
+                        g = 255; 
+                    } else
+                    {
+                        g = greenS; 
+                    } 
+
+                    if(blueS > 255)
+                    {
+                        b = 255; 
+                    } else
+                    {
+                        b = blueS; 
+                    }
+
+
+                    resultimage.SetPixel(x, y, Color.FromArgb(r, g, b));
+                }
+            }
+            pictureBox2.Image = resultimage;
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
